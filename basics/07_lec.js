22c7561196fbe037f7a1114a84f40ceb10dc2f28
Atul-Gupta-86 <vittanathcoder@gmail.com>
2025-11-14 19:30:28 +0000

    operations

---
+++ basics/07_lec.js
@@ -0,0 +1,32 @@
+let value = 3
+let negative_value = -value
+console.log(negative_value) 
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(3/2) // give decimal values.
+console.log(2%3)
+
+let string1 = "Atul"
+let string2 = " Gupta"
+let string3 = string1+string2
+console.log(string3)
+
+
+/*Never recommanded 
+console.log("2"+2)
+console.log(2+"2")
+console.log("2"+2+3)
+console.log(2+3+"2")
+
+
+console.log(+"")    // 0
+console.log(+true)  // 1
+
+*/
+
+let game_score =100
+console.log(game_score++)
+console.log(++game_score)
